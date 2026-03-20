b17e8e397922a4da7667fecc56636a495767bcba
Gauthier Bach <gabach@z2r10p5.42lyon.fr>
2026-03-20 15:40:50 +0100

    added test for the parser and finished it

---
+++ tests/parser/test_custom_maps.py
@@ -0,0 +1,45 @@
+from src.features.parser import MapData
+
+
+def valid_maps_test_parsing() -> bool:
+
+    MAPS: list[str] = [
+            "tests/parser/valid_maps/map_1.txt",
+            "tests/parser/valid_maps/map_2.txt",
+            "tests/parser/valid_maps/map_3.txt",
+            "tests/parser/valid_maps/map_4.txt",
+            "tests/parser/valid_maps/map_5.txt",
+            "tests/parser/valid_maps/map_6.txt",
+            "tests/parser/valid_maps/map_7.txt",
+            "tests/parser/valid_maps/map_8.txt",
+            "tests/parser/valid_maps/map_9.txt",
+            "tests/parser/valid_maps/map_10.txt",
+            ]
+
+    return_value: bool = True
+
+    print()
+    print()
+    print("Starting Test for Custom Valid Maps")
+
+    for subject_map in MAPS:
+        print()
+        print(f"\033[95mTEST \"{subject_map}\":\033[0m")
+
+        try:
+            map_data: MapData = MapData()
+
+            map_data.parsing(subject_map)
+
+            print(map_data)
+
+        except Exception as err:
+            print(f"\033[93m/!\\ {err.__class__.__name__}: {err}\033[0m")
+
+            return_value = False
+
+    return return_value
+
+
+if __name__ == "__main__":
+    valid_maps_test_parsing()
---
+++ tests/parser/test_parser.py
@@ -0,0 +1,36 @@
+from .test_subject_maps import subject_maps_test_parsing
+from .test_custom_maps import valid_maps_test_parsing
+from .test_custom_false_maps import invalid_maps_test_parsing
+
+
+def test_parsing() -> None:
+    print("""
+choose the test you want to do:
+    -> subject maps        (1)
+    -> valid custom maps   (2)
+    -> invalid custom maps (3)
+    -> all                 (4)
+    """)
+
+    choice: str = input("choice: ")
+
+    if choice == "1":
+        subject_maps_test_parsing()
+
+    elif choice == "2":
+        valid_maps_test_parsing()
+
+    elif choice == "3":
+        invalid_maps_test_parsing()
+
+    elif choice == "4":
+        subject_maps_test_parsing()
+        valid_maps_test_parsing()
+        invalid_maps_test_parsing()
+
+    else:
+        print("/!\\ INVALID choice")
+
+
+if __name__ == "__main__":
+    test_parsing()
