9b3693fc5964e3f757c0055e894dfa90562c9ecf
Gauthier Bach <gabach@z2r11p7.42lyon.fr>
2026-04-03 16:45:15 +0200

    qwe

--- src/features/visualizer/pannable_scroll_area.py
+++ src/features/visualizer/pannable_scroll_area.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import QScrollArea
 from PyQt6.QtCore import Qt, QPoint
-from PyQt6.QtGui import QCursor
+from PyQt6.QtGui import QCursor, QMouseEvent
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -10,6 +10,7 @@ if TYPE_CHECKING:
 class PannableScrollArea(QScrollArea):
     def __init__(self, parent: "MainWindow") -> None:
         super().__init__(parent)
+
         self.setHorizontalScrollBarPolicy(
                 Qt.ScrollBarPolicy.ScrollBarAlwaysOff
                 )
@@ -17,16 +18,23 @@ class PannableScrollArea(QScrollArea):
         self._drag_start = None
         self._scroll_start = None
 
-    def mousePressEvent(self, event) -> None:
+    def mousePressEvent(self, event: QMouseEvent | None) -> None:
+        if event is None:
+            return
         if event.button() == Qt.MouseButton.LeftButton:
             self._drag_start = event.globalPosition().toPoint()
-            self._scroll_start = QPoint(
-                self.horizontalScrollBar().value(),
-                self.verticalScrollBar().value()
-            )
+            horizontalScrollBar = self.horizontalScrollBar()
+            verticalScrollBar = self.verticalScrollBar()
+            if horizontalScrollBar and verticalScrollBar:
+                self._scroll_start = QPoint(
+                    horizontalScrollBar.value(),
+                    verticalScrollBar.value()
+                )
             self.setCursor(QCursor(Qt.CursorShape.ClosedHandCursor))
 
-    def mouseMoveEvent(self, event) -> None:
+    def mouseMoveEvent(self, event: QMouseEvent | None) -> None:
+        if event is None:
+            return
         if (
                 self._drag_start is not None and
                 hasattr(self._scroll_start, "x") and
@@ -40,7 +48,9 @@ class PannableScrollArea(QScrollArea):
                     self._scroll_start.y() - delta.y()
                     )
 
-    def mouseReleaseEvent(self, event) -> None:
+    def mouseReleaseEvent(self, event: QMouseEvent | None) -> None:
+        if event is None:
+            return
         self._drag_start = None
         self._scroll_start = None
         self.setCursor(QCursor(Qt.CursorShape.ArrowCursor))
